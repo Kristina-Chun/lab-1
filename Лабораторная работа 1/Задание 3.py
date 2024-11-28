7782f45ebe20ccce7a6b92ef20335b8699869d6e
Kristina-Chun <158212661+Kristina-Chun@users.noreply.github.com>
2024-11-28 12:17:14 +0300

    Update Задание 3.py

--- Лабораторная работа 1/Задание 3.py
+++ Лабораторная работа 1/Задание 3.py
@@ -1,10 +1,10 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
-disk_size = 1.44 #Мб
-pages = 100
-lines = 50
-symbols_per_line = 25
-symbol_weight = 4 #байт
-book_size = pages * lines * symbols_per_line * symbol_weight #байт
-mb_book_size = book_size / 1024**2 #Мб
-number_of_books = disk_size//mb_book_size
-print("Количество книг, помещающихся на дискету:", round(number_of_books))
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# индекс середины
+middle_index = len(list_players) // 2
+
+first_team = list_players[:middle_index]
+second_team = list_players[middle_index:]
+
+print(first_team)
+print(second_team)
